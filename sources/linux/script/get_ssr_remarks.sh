476a3288404ec3744017378bc8d08a22fe55aef0
tangyong <tangyong@debian9>
2020-03-30 17:02:33 +0800

    decode ssr url of base64, and show remarks

---
+++ sources/linux/script/get_ssr_remarks.sh
@@ -0,0 +1,50 @@
+if [ "a$1" == "a" ] ; then
+  echo "no arguments"
+  exit 0
+fi
+
+dessru=$1
+ssu=${dessru#ssr://}
+
+strcoded=`echo "$ssu" | sed "s/-/+/g" | sed "s/_/\//g"`
+
+lencoded=${#strcoded};
+remind=$((lencoded%4));
+
+while [ "$remind" != "0" ]
+do
+	strcoded="$strcoded=";
+	remind=$((remind+1));
+	remind=$((remind%4));
+done
+
+
+dss=`echo $strcoded | openssl base64 -d`
+#echo $dss
+
+rmk=${dss#*remarks=}
+
+#echo $rmk
+
+emk=${rmk%%&*}
+
+#echo $emk
+
+emk=`echo "$emk" | sed "s/-/+/g" | sed "s/_/\//g"`
+
+mlen=${#emk}
+mlea=$((mlen%4))
+while [ "$mlea" != "0" ]
+do
+        emk="$emk="
+        mlea=$((mlea+1))
+        mlea=$((mlea%4))
+done
+
+echo "----"
+echo $emk | openssl base64 -d
+echo "  "
+echo $ssu
+
+
+
