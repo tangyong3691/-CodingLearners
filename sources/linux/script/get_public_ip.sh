c1d7448bd3d211ac697782cd1a0633af13790d35
tangyong <tangyong@debianty.localhost>
2021-11-27 15:21:17 +0800

    add get public info, save to files

---
+++ sources/linux/script/get_public_ip.sh
@@ -0,0 +1,34 @@
+if [ "a$1" == "a" ] ; then
+ipfile="/dataext/soft/myip_infos"
+else
+ipfile=$1
+fi
+
+echo "$ipfile"
+
+if ! [ -e $ipfile ] ; then
+echo "create $ipfile"
+touch $ipfile
+fi
+
+echo "check loop enable file: /dataext/soft/loopok"
+while [ -f /dataext/soft/loopok ] 
+do
+currentip=`dig +short myip.opendns.com @resolver1.opendns.com`
+
+if [ "a$currentip" == "a" ] ; then
+thisdate=`date`
+echo "get ip error , $thisdate"
+fi
+
+chkit=`cat $ipfile | grep $currentip`
+
+if [ "a$chkit" == "a" ] ; then
+echo "add ip: $currentip"
+ipadddate=`date`
+echo "$currentip   $ipadddate" >> $ipfile
+fi
+
+sleep 3600
+done
+
